2b000992859604b787f51e392b52615f7b86ddf4
amit-agrawal10 <amit.agrawal10@gmail.com>
2020-10-29 07:33:56 +0530

    Moved process orchestrator model and repository to separate project

---
+++ src/main/java/com/github/amag/processorchestrator/ProcessOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.github.amag.processorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessOrchestratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessOrchestratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/amag/processorchestrator/ProcessOrchestratorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github.amag.processorchestrator;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProcessOrchestratorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
