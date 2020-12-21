688d5c509a2ce8ac7b6d09bd7e4ceb718a9f90dc
amit-agrawal10 <amit.agrawal10@gmail.com>
2020-12-21 11:01:01 +0530

    Committing tested code 5

--- src/main/java/com/github/amag/processorchestrator/scheduler/ProcessTaskScheduler.java
+++ src/main/java/com/github/amag/processorchestrator/scheduler/ProcessTaskScheduler.java
@@ -13,17 +13,17 @@ public class ProcessTaskScheduler {
 
     private final TaskManager taskManager;
 
-    @Scheduled(initialDelayString = "${amag.task.job.start.initial.delay:70000}",fixedRateString = "${amag.task.job.start.delay:500}")
+    @Scheduled(fixedRateString = "${amag.task.job.start.delay:500}")
     protected void startTask() {
         taskManager.startTask();
     }
 
-    @Scheduled(initialDelayString = "${amag.task.job.ready.initial.delay:75000}",fixedRateString = "${amag.task.job.ready.delay:500}")
+    @Scheduled(fixedRateString = "${amag.task.job.ready.delay:500}")
     protected void findReadyTask() {
         taskManager.findAndMarkReadyTask();
     }
 
-    @Scheduled(initialDelayString = "${amag.task.job.execute.initial.delay:80000}",fixedRateString = "${amag.task.job.execute.delay:500}")
+    @Scheduled(fixedRateString = "${amag.task.job.execute.delay:500}")
     protected void executeTask() {
         taskManager.executeTask();
     }
